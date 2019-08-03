3c3715971d557261d2284674ec3bb1bba21b57b9
Hypersonic_RR <ronex665@gmail.com>
2019-08-03 09:21:34 +0530

    First Commit

---
+++ src/components/Bio.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import profilePic from '../assets/profile-pic.jpg';
+import { rhythm } from '../utils/typography';
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          marginBottom: rhythm(2),
+        }}
+      >
+        <h2>News Blog for Maverick</h2>
+      </div>
+    );
+  }
+}
+
+export default Bio;
---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { rhythm } from '../utils/typography';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer
+        style={{
+          marginTop: rhythm(2.5),
+          paddingTop: rhythm(1),
+        }}
+      >
+        Carmel Junior College,JSR
+              </footer>
+    );
+  }
+}
+
+export default Footer;
